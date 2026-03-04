54ee82bdf5361a52014192f7c7fa8b415338321a
salman <mrsalmanxzs@gmail.com>
2026-03-04 13:49:32 +0530

    asi.py file updated

--- backend_core/saloon_core/asgi.py
+++ backend_core/saloon_core/asgi.py
@@ -1,31 +1,23 @@
 """
 ASGI config for saloon_core project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saloon_core.settings')
-
+# Initialize Django ASGI application early to ensure the AppRegistry is populated
 django_asgi_app = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import AllowedHostsOriginValidator
 from bookings.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter(
-                websocket_urlpatterns
-            )
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns  
         )
     ),
 })
